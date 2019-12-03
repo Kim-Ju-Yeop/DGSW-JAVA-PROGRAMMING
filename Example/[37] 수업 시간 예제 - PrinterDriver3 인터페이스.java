b2e32bf076d23b50889992584181375749a42f55
juyeop <kjy031104@gmail.com>
2019-12-03 18:50:50 +0900

    JAVA-PROGRAMMING

---
+++ Example/[37] 수업 시간 예제 - PrinterDriver3 인터페이스.java
@@ -0,0 +1,37 @@
+package Ex_interface;
+
+interface Printable3{
+    void print(String doc);
+}
+
+interface ColorPrintable3 extends Printable3{
+    void printCMYK(String doc);
+}
+
+class Prn909Drv2 implements ColorPrintable3{
+
+    @Override
+    public void print(String doc){
+        System.out.println("From MD-909 black & white ver");
+        System.out.println(doc);
+    }
+
+    @Override
+    public void printCMYK(String doc){
+        System.out.println("From MD-909 CMYK ver");
+        System.out.println(doc);
+    }
+}
+
+public class PrinterDriver3 {
+    public static void main(String[] args) {
+        String myDoc = "This is a report about...";
+
+        ColorPrintable3 prn = new Prn909Drv2();
+        prn.print(myDoc);
+
+        System.out.println();
+
+        prn.printCMYK(myDoc);
+    }
+}
